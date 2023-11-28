b5df24a426706523b917c757883343c5302cb285
Vasilii <88195281+Vaskis23@users.noreply.github.com>
2023-11-28 17:26:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let players = [
+    {
+        name: "player777",
+        rating: 4.5,
+        xp: 1_000_000,
+        skills: ["fast", "exact", "silent"]
+    },
+
+    {
+        name: "marry",
+        rating: 5.0,
+        xp: 2_000_000,
+        skills: ["mvp", "silent"]
+    },
+
+    {
+        name: "petersOn",
+        rating: 2.5,
+        xp: 1_000,
+        skills: ["fast", "exact", "silent"]
+    },
+]
